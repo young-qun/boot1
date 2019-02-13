0234da163255a1f7b2a5330a62cda77de8ba2154
young-qun <1533043066@qq.com>
2019-02-13 15:47:52 +0800

    git 提交到本地

---
+++ src/test/java/com/ssm/young/bootdemo/BootdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ssm.young.bootdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
